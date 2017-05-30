18015744c397e0c584abd6e853a0dcb106026de9
tedc <egrandinetti@gmail.com>
2017-05-30 11:42:44 +0200

    prova

--- templates/header-gallery.php
+++ templates/header-gallery.php
@@ -4,7 +4,7 @@
 			$full = is_mobile() ? $image['sizes']['large'] : $image['url'];
 			$large = wp_get_attachment_image( $image['ID'], 'large', false, array('class' => 'thumb--hidden'));
 		?>
-	<div class="slider__item" style="background-image: url(<?php echo $full; ?>);"<?php scrollmagic('"tween":{"backgroundPosition": "50% 0%"},"triggerHook":0,"offeset":-80,"triggerElement":"#header","duration":"100vh"'); ?>>
+	<div class="slider__item" style="background-image: url(<?php echo $full; ?>);"<?php //scrollmagic('"tween":{"backgroundPosition": "50% 0%"},"triggerHook":0,"offeset":-80,"triggerElement":"#header","duration":"100vh"'); ?>>
 		<?php echo $large; ?>
 	</div>
 	<?php endforeach; ?>
