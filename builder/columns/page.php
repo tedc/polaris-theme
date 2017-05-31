41c7dad7e5fd596e60655eb8466396dcb979ba03
tedc <egrandinetti@gmail.com>
2017-05-31 10:31:58 +0200

    animazione

--- builder/columns/page.php
+++ builder/columns/page.php
@@ -1,10 +1,10 @@
 <?php 
 	$page = get_sub_field('page');
  $mv = ($col%2==0) ? '"80"' : '"-80"'; ?>
-<div class="section__content <?php echo ($is_anim) ? 'section__content--anim ' : ''; ?>section__content--grow-lg section__content--shrink section__content--shrink<?php echo ($col%2==0) ? '-right-double' : '-left-double'; ?>"<?php scrollmagic('"tween":{"y":'. $mv . '},"triggerElement":"#col_'.$col.'_'.$row.'", "triggerHook":1,"duration":"150vh"'); ?>>
-	<h4 class="section__title--huge-upper"><strong><?php echo get_the_title($page->ID); ?></strong></h4>
+<div class="section__content <?php echo ($is_anim) ? 'section__content--anim ' : ''; ?>section__content--grow-lg section__content--shrink section__content--shrink<?php echo ($col%2==0) ? '-right-double' : '-left-double'; ?>">
+	<h4 class="section__title--huge-upper"<?php scrollmagic('"tween":{"y":'. $mv . '},"triggerElement":"#col_'.$col.'_'.$row.'", "triggerHook":1,"duration":"150vh"'); ?>><strong><?php echo get_the_title($page->ID); ?></strong></h4>
 	<?php echo '<p>'.$page->post_excerpt.'</p>'; ?>
-	<div class="section__button section__button--grow-md-top">
+	<div class="section__button section__button--grow-md-top"<?php scrollmagic('"tween":{"y":'. $mv . '},"triggerElement":"#col_'.$col.'_'.$row.'", "triggerHook":1,"duration":"150vh"'); ?>>
 		<a href="<?php echo get_permalink($page->ID); ?>" class="button">
 			<span class="button__label"><?php _e('Scopri di più', 'polaris'); ?></span>
 			<i class="icon-arrow-big"></i>
