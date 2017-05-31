7f4713e6c31e153b9f9b87d012c5a1927d2a5abe
tedc <egrandinetti@gmail.com>
2017-05-31 10:29:49 +0200

    prova animazione colonne

--- builder/columns/page.php
+++ builder/columns/page.php
@@ -1,7 +1,7 @@
 <?php 
 	$page = get_sub_field('page');
  $mv = ($col%2==0) ? '"80"' : '"-80"'; ?>
-<div class="section__content section__content--grow-lg section__content--shrink section__content--shrink<?php echo ($col%2==0) ? '-right-double' : '-left-double'; ?>"<?php scrollmagic('"tween":{"y":'. $mv . '},"triggerElement":"#col_'.$col.'_'.$row.'", "triggerHook":1,"duration":"150vh"'); ?>>
+<div class="section__content <?php echo ($is_anim) ? 'section__content--anim ' : ''; ?>section__content--grow-lg section__content--shrink section__content--shrink<?php echo ($col%2==0) ? '-right-double' : '-left-double'; ?>"<?php scrollmagic('"tween":{"y":'. $mv . '},"triggerElement":"#col_'.$col.'_'.$row.'", "triggerHook":1,"duration":"150vh"'); ?>>
 	<h4 class="section__title--huge-upper"><strong><?php echo get_the_title($page->ID); ?></strong></h4>
 	<?php echo '<p>'.$page->post_excerpt.'</p>'; ?>
 	<div class="section__button section__button--grow-md-top">
