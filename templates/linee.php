0481a2ddaa33d7dbe0c3542e2db3315c7943ac36
tedc <egrandinetti@gmail.com>
2017-05-31 18:14:00 +0200

    various

--- templates/linee.php
+++ templates/linee.php
@@ -1,5 +1,5 @@
 <ul class="menu menu--linee menu--shrink menu--grow-lg">
-	<li class="menu__item menu__item--title"><h3 class="menu__title menu__title--small"><?php _e('Collezione', 'polaris'); ?></h3></li>
+	<li class="menu__item menu__item--title"><h3 class="menu__title menu__title--small"><?php _e('Collezioni', 'polaris'); ?></h3></li>
 	<?php foreach (get_posts(array('post_type'=>'linee', 'posts_per_page'=>-1,'orderby'=>'menu_order')) as $linea) {
 	$current = (is_singular('linee') && $post->ID == $linea->ID) ? ' menu__item--active' : ''; 
 	echo '<li class="menu__item menu__item--'.$linea->post_name.$current.'"><a href="'.get_permalink($linea->ID).'">'.get_the_title($linea->ID).'</a></li>';
