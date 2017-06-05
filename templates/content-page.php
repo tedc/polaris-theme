75a073ce154b646b8ee6e90e4c01281a4f1ad30a
tedc <egrandinetti@gmail.com>
2017-06-05 19:22:21 +0200

    grow page

--- templates/content-page.php
+++ templates/content-page.php
@@ -2,10 +2,10 @@
 <?php the_content(); ?>
 <?php else : ?>
 
-<header class="page__header page__header--grow-top page__header--mw">
+<header class="page__header page__header--grow page__header--mw">
 	<h1 class="page__title page__title--normal"><?php the_title(); ?></h1>
 </header>
-<div class="page__content page__content--mw page__content--grow-lg">
+<div class="page__content page__content--mw page__content--grow">
 <?php the_content(); ?>
 </div>
 <?php endif ; ?>
