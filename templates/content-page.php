87ec58540bc2853f28376dbee861d42df0d7ae23
tedc <egrandinetti@gmail.com>
2017-06-05 19:20:31 +0200

    page normal

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,1 +1,11 @@
+<?php if(is_page_template( 'template-custom.php' )) : ?>
 <?php the_content(); ?>
+<?php else : ?>
+
+<header class="page__header">
+	<h1 class="page__title page__title--normal"><?php the_title(); ?></h1>
+</header>
+<div class="page__content page__content--mw page__content--grow-lg">
+<?php the_content(); ?>
+</div>
+<?php endif ; ?>
