fd175be9c4b110e80d53ef052122a98b61446a44
tedc <egrandinetti@gmail.com>
2017-06-05 19:21:26 +0200

    page__header

--- templates/content-page.php
+++ templates/content-page.php
@@ -2,7 +2,7 @@
 <?php the_content(); ?>
 <?php else : ?>
 
-<header class="page__header">
+<header class="page__header page__header--grow-top page__header--mw">
 	<h1 class="page__title page__title--normal"><?php the_title(); ?></h1>
 </header>
 <div class="page__content page__content--mw page__content--grow-lg">
