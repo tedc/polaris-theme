9b3802237115391a462a052163b1eff320c42e32
tedc <egrandinetti@gmail.com>
2017-06-05 19:23:52 +0200

    page fix

--- templates/content-page.php
+++ templates/content-page.php
@@ -2,10 +2,10 @@
 <?php the_content(); ?>
 <?php else : ?>
 
-<header class="page__header page__header--grow page__header--mw">
-	<h1 class="page__title page__title--normal"><?php the_title(); ?></h1>
+<header class="page__header page__header--grow-md-top page__header--mw">
+	<h1 class="page__title page__title--big"><?php the_title(); ?></h1>
 </header>
-<div class="page__content page__content--mw page__content--grow">
+<div class="page__content page__content--mw page__content--grow-md">
 <?php the_content(); ?>
 </div>
 <?php endif ; ?>
