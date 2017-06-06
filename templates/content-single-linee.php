46434a39beb811db449dc40b9f85aab5415938b9
tedc <egrandinetti@gmail.com>
2017-06-06 12:39:43 +0200

    content

--- templates/content-single-linee.php
+++ templates/content-single-linee.php
@@ -1,4 +1,4 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/'. get_post_type(), 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <?php get_template_part('templates/content', 'linee'); ?>
 <?php endwhile; ?>
