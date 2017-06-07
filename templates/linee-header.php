ff979d338c21c2e42e92ee01d1151cca44cd28fa
tedc <egrandinetti@gmail.com>
2017-06-07 11:48:25 +0200

    linee header

--- templates/linee-header.php
+++ templates/linee-header.php
@@ -2,7 +2,7 @@
 	use Roots\Sage\Titles; 
 	$kind = get_field('header_kind');
 ?>
-<header id="header" class="linee-header linee-header--full linee-header--grid">
+<header id="header" class="linee-header linee-header--full linee-header--grid"<?php scrollmagic('"class":"linee--active","triggerHook":0.75,"duration":0,"reverse":false'); ?>>
 	<style>
 		.linee-header__cell--figure {
 			background-image: url(<?php the_post_thumbnail_url('large'); ?>);
@@ -20,5 +20,6 @@
 		<h1 class="linee-header__title linee-header__title--huge-upper<?php echo (get_field('long_title')) ? ' linee-header__title--dm' : ''; ?>"><strong<?php scrollmagic('"tween":{"y":-80},"triggerElement":"#header","triggerHook":0,"duration":"100vh","offset":"-80"'); ?>><?php echo Titles\title(); ?></strong></h1>
 		<?php the_field('description_text'); ?>
 	</div>
+	<div class="linee-header__mask"></div>
 </header>
 <hr id="down" />
