37e5386eeb8c5a46c14b15d416e041bb4231d0b9
tedc <egrandinetti@gmail.com>
2017-06-07 11:05:54 +0200

    polaris wpsl

--- extras/wpsl.php
+++ extras/wpsl.php
@@ -20,3 +20,22 @@
 	    
 	    return $marker_props;
 	}
+
+	add_filter( 'wpsl_templates', 'custom_templates' );
+
+	function custom_templates( $templates ) {
+
+	    /**
+	     * The 'id' is for internal use and must be unique ( since 2.0 ).
+	     * The 'name' is used in the template dropdown on the settings page.
+	     * The 'path' points to the location of the custom template,
+	     * in this case the folder of your active theme.
+	     */
+	    $templates[] = array (
+	        'id'   => 'polaris-wpsl',
+	        'name' => 'Polaris WPSL',
+	        'path' => get_stylesheet_directory() . '/' . 'templates/custom-wpsl.php',
+	    );
+
+	    return $templates;
+	}
