2cd17b67382a139576d3d183e0be58614e1949c4
tedc <egrandinetti@gmail.com>
2017-06-07 12:06:37 +0200

    ICL_DONT_LOAD_NAVIGATION_CSS

--- extras/wpml.php
+++ extras/wpml.php
@@ -69,3 +69,7 @@
         $langs .= '</div>';
         //echo $langs;
     }
+
+    define('ICL_DONT_LOAD_NAVIGATION_CSS', true);
+    define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
+    define('ICL_DONT_LOAD_LANGUAGES_JS', true);
