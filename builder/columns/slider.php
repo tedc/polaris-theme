2c199672a798160a0c0eab92e8bce9fde5d22137
tedc <egrandinetti@gmail.com>
2017-07-05 19:12:02 +0200

    section__slider--shrink

--- builder/columns/slider.php
+++ builder/columns/slider.php
@@ -2,7 +2,7 @@
 $images = get_sub_field('slider');
 $full = false;
 $row = $row .'_'.$col;
-echo '<div id="slider_'.$row.'" class="section__slider section__slider--'.get_sub_field('background_size').'">';
+echo '<div id="slider_'.$row.'" class="section__slider section__slider--shrink section__slider--'.get_sub_field('background_size').'">';
 include(locate_template( 'builder/commons/gallery.php', false, true )); 
 $mask = ($col%2==0) ? 'left' : 'right';
 echo '</div>';
