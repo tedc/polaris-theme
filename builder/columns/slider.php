8db9f193a7ccb2421880651c26725653f4639277
tedc <egrandinetti@gmail.com>
2017-07-05 19:01:40 +0200

    section__slider

--- builder/columns/slider.php
+++ builder/columns/slider.php
@@ -2,7 +2,9 @@
 $images = get_sub_field('slider');
 $full = false;
 $row = $row .'_'.$col;
-echo '<div id="slider_'.$row.'">';
+echo '<div id="slider_'.$row.'" class="section__slider section__slider--'.get_sub_field('background_size').'">';
 include(locate_template( 'builder/commons/gallery.php', false, true )); 
+$mask = ($col%2==0) ? 'left' : 'right';
 echo '</div>';
+echo '<div class="section__mask section__mask--'.$mask.'"></div>';
 ?>
