e930849a13eed80e87f37fda760ba4a4f32054ac
tedc <egrandinetti@gmail.com>
2017-07-05 18:54:47 +0200

    slider

--- builder/columns/slider.php
+++ builder/columns/slider.php
@@ -2,4 +2,7 @@
 $images = get_sub_field('slider');
 $full = false;
 $row = $row .'_'.$col;
-include(locate_template( 'builder/commons/gallery.php', false, true )); ?>
+echo '<div id="slider_'.$row.'">';
+include(locate_template( 'builder/commons/gallery.php', false, true )); 
+echo '</div>';
+?>
