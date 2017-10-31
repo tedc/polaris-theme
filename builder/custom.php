8339aa30cccc3fd5f73ecaa430898ef9a90c2a0f
tedc <egrandinetti@gmail.com>
2017-10-31 10:25:32 +0100

    custom_slider_

--- builder/custom.php
+++ builder/custom.php
@@ -16,12 +16,12 @@ $responsive = ', "responsive":[{"breakpoint" : 850,"settings" :{"slidesToShow": 
 		$count = (count(get_sub_field('item_images')) > 2) ? 3 : 2;
 		$center = (count(get_sub_field('item_images')) > 3) ? '"centerMode" : true,' : '';
 	?>
-		<div class="custom__cell" data-title="<?php the_sub_field('item_title'); ?>" id="<?php echo sanitize_title(get_sub_field('item_title')); ?>_<?php echo $item; ?>">
+		<div class="custom__cell" data-title="<?php the_sub_field('item_title'); ?>" id="custom_slider_<?php echo $item; ?>">
 		<div class="custom__content custom__content--shrink custom__content--grow-md">
 			<?php the_sub_field('item_text'); ?>
 		</div>
 	<div class="custom__bottom">
-	<div class="custom__carousel<?php echo (count(get_sub_field('item_images')) > 3) ? ' custom__carousel--centered' : ''; ?>" data-slick='{"slidesToShow": <?php echo $count; ?>, "slidesToScroll": 1,<?php echo $center; ?>"infinite":true, "speed" : 300, "cssEase" : "linear","prevArrow":"#<?php echo sanitize_title(get_sub_field('item_title')); ?>_<?php echo $item; ?> .custom__arrow--prev","nextArrow":"#<?php echo sanitize_title(get_sub_field('item_title')); ?>_<?php echo $item; ?>  .custom__arrow--next", "responsive":[{"breakpoint" : 640,"settings" :{"slidesToShow": 1}}]}'>
+	<div class="custom__carousel<?php echo (count(get_sub_field('item_images')) > 3) ? ' custom__carousel--centered' : ''; ?>" data-slick='{"slidesToShow": <?php echo $count; ?>, "slidesToScroll": 1,<?php echo $center; ?>"infinite":true, "speed" : 300, "cssEase" : "linear","prevArrow":"#custom_slider__<?php echo $item; ?> .custom__arrow--prev","nextArrow":"#custom_slider__<?php echo $item; ?>  .custom__arrow--next", "responsive":[{"breakpoint" : 640,"settings" :{"slidesToShow": 1}}]}'>
 	<?php foreach(get_sub_field('item_images') as $img) : ?>
 		<figure class="custom__figure custom__figure--shrink custom__figure--grow">
 			<div class="custom__figure-wrapper">
